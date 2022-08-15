8aaf51aab48e2cab06c7c4cfad0a613f269cc198
KlimeSE <68099172+KlimeSE@users.noreply.github.com>
2022-08-15 18:08:38 +0100

    Initial Commit

---
+++ BuildFreedom.cs
@@ -0,0 +1,33 @@
+﻿using Sandbox.Engine.Utils;
+using Sandbox.Game;
+using Sandbox.Game.World;
+using Sandbox.ModAPI;
+using System;
+using System.Collections.Generic;
+using VRage;
+using VRage.Game;
+using VRage.Game.Components;
+using VRage.Game.ModAPI;
+using VRage.Plugins;
+using VRage.Utils;
+
+namespace BuildFreedom
+{
+    public class BuildFreedom : IPlugin, IDisposable
+    {
+        public void Init(object gameInstance)
+        {
+            MyFakes.DISABLE_CLIPBOARD_PLACEMENT_TEST = true;
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            MyFakes.DISABLE_CLIPBOARD_PLACEMENT_TEST = false;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# BuildFreedom
+Building tools for SE
+
+## Installation
+
+- Add this plugin via the Plugin Loader: https://github.com/sepluginloader/PluginLoader
+
+## Server Information
+
+- This plugin is not required serverside.
